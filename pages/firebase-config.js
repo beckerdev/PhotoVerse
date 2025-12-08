75d79068ba3d452c9796af4ef9b732b8edcdeed3
Cassiano Bittencourt Becker <cassiano.becker@institutoivoti.com.br>
2025-12-08 18:07:40 +0000

    glmwrfb

--- pages/firebase-config.js
+++ pages/firebase-config.js
@@ -1,12 +1,3 @@
-// Cole aqui suas credenciais do Firebase (substitua pelos valores do seu projeto)
-export const firebaseConfig = {
-  apiKey: "AIzaSyB3g82Vs7HxeWUb4VN-gvqnOFLVnNnE-VE",
-  authDomain: "photoverse-53852.firebaseapp.com",
-  projectId: "photoverse-53852",
-  storageBucket: "photoverse-53852.appspot.com",
-  messagingSenderId: "291140576216",
-  appId: "1:291140576216:web:ecea91c3e8d179731288ad",
-  measurementId: "G-1Z8P7FJXC3"
-};
-
-// IMPORTANTE: nunca comite chaves reais em repositórios públicos.
+// Firebase removido: este arquivo não é mais usado.
+// A galeria agora salva as imagens localmente via IndexedDB.
+export const firebaseConfig = null;
